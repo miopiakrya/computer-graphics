5ec5a2b25daeae44137809c6f9373ea498921c73
Близорукость <90715911+miopiakrya@users.noreply.github.com>
2021-09-21 21:54:56 +0300

    Lab1. CompGraf

---
+++ Lab1.CompGraf.B/Lab1.CompGraf.B/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1.CompGraf.B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+            protected override void OnPaint(PaintEventArgs e)
+            {
+
+            // Получаем объект Graphics
+            Graphics g = e.Graphics;
+            g.DrawLine(Pens.Red, 20, 10, 220, 30);
+            g.DrawEllipse(Pens.Blue, 20, 40, 220, 90);
+            g.DrawRectangle(Pens.Green, 20, 140, 220, 90);
+            g.FillEllipse(Brushes.Blue, 260, 40, 220, 90);
+            g.FillRectangle(Brushes.Green, 260, 140, 220, 90);
+            base.OnPaint(e);
+
+            }
+    }
+
+    
+}
---
+++ Lab1.CompGraf/Lab1.CompGraf/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1.CompGraf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen myPen = new Pen(Color.Red, 1);
+            Graphics g = pictureBox1.CreateGraphics();
+            g.DrawRectangle(myPen, 0, 0, pictureBox1.Size.Width - 1,
+            pictureBox1.Size.Height - 1);
+            g.Dispose();
+        }
+    }
+}
