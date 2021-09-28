aeb05ca0b53c9f9961f4456c3225ebbf7c196c47
Близорукость <90715911+miopiakrya@users.noreply.github.com>
2021-09-28 18:41:57 +0300

    CompGraf

---
+++ Lab2/LR2/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace LR2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(150, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Pixels";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Pixels_Output);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(300, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 32);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Inches";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Inches_Output);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(450, 406);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Millimeter";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Millimeters_Output);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(600, 406);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 32);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Очистить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Clear_PictureBox);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Lab2/LR2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LR2
+{
+    public partial class Form1 : Form
+    {
+        // Объявляем объект "g" класса Graphics
+        Graphics g;
+        //**************Конструктор формы *******************************
+        public Form1()
+        {
+            InitializeComponent();
+            // Предоставляем объекту "g" класса Graphics
+            // возможность рисования на pictureBox1:
+            g = pictureBox1.CreateGraphics();
+        }
+        // ********* Стандарт страничного пространства – Pixel ********
+        private void Pixels_Output(object sender, EventArgs e)
+        {
+            // экранные координаты
+            int ex = 0, ey = 0, old_ex = 0, old_ey = 0;
+            // математические значения
+            double x = 0, y = 0;
+            // цвет осей и рамки
+            Pen axesPen = new Pen(Color.Red, 1);
+            // цвет графика
+            Pen graphicsPen = new Pen(Color.FromArgb(0, 255, 0), 1);
+            // Устанавливаем фон pictureBox1 в именованный цвет
+            pictureBox1.BackColor = Color.FromName("Azure");
+            pictureBox1.Refresh();
+            // Стандарт страничного пространства в Pixel
+            g.PageUnit = GraphicsUnit.Pixel;
+            //Рисуем прямоугольник:
+            g.DrawRectangle(axesPen, 0, 0,
+            pictureBox1.Size.Width - 1, pictureBox1.Size.Height - 1);
+            // Рисуем оси
+            g.DrawLine(axesPen, 0, (pictureBox1.Size.Height - 1) / 2,
+            pictureBox1.Size.Width - 1, (pictureBox1.Size.Height - 1) / 2);
+            g.DrawLine(axesPen, (pictureBox1.Size.Width - 1) / 2, 0,
+            (pictureBox1.Size.Width - 1) / 2, pictureBox1.Size.Height - 1);
+            // Рисуем график функции y=Sin(x)
+            x = - Math.PI * 2;
+            for (ex = 0; ex <= 1000; ex++)
+            {
+                y = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + x;
+                ey = (pictureBox1.Size.Height - 1) - (Convert.ToInt32(y * 200) + 200);
+                if (ex != 0) { g.DrawLine(graphicsPen, old_ex, old_ey, ex, ey); }
+                old_ex = ex; old_ey = ey;
+                x = x + (Math.PI * 4) / (pictureBox1.Size.Width - 1);
+            }
+        }
+
+        // ******** Стандарт страничного пространства – Millimeter *****
+        private void Millimeters_Output(object sender, EventArgs e)
+        {
+            int ex = 0, ey = 0, old_ex = 0, old_ey = 0;
+            double x = 0, y = 0;
+            // Стандарт страничного пространства Millimeter
+            g.PageUnit = GraphicsUnit.Millimeter;
+            Pen axesPen = new Pen(Color.Cyan, 0.1f);
+            Pen graphicsPen = new Pen(Color.FromArgb(0, 0, 255), 0.1f);
+            // Устанавливаем фон pictureBox1 в именованный цвет
+            pictureBox1.BackColor =
+
+            Color.FromKnownColor(KnownColor.ControlLightLight);
+
+            pictureBox1.Refresh();
+            //Получаем ширину и высоту прямоугольника в миллиметрах
+            int WidthInMM = Convert.ToInt16((pictureBox1.Size.Width - 1) /
+            g.DpiX * 25.4);
+            int HeightInMM = Convert.ToInt16((pictureBox1.Size.Height - 1) /
+            g.DpiY * 25.4);
+
+            //Рисуем прямоугольник
+            g.DrawRectangle(axesPen, 0, 0, WidthInMM, HeightInMM);
+            // Рисуем оси
+            g.DrawLine(axesPen, 0, HeightInMM / 2, WidthInMM, HeightInMM / 2);
+            g.DrawLine(axesPen, WidthInMM / 2, 0, WidthInMM / 2, HeightInMM);
+            // Рисуем график функции y=Sin(x)
+            x = -Math.PI * 2;
+            for (ex = 0; ex <= WidthInMM; ex++)
+            {
+                y = Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + x;
+                ey = HeightInMM - (Convert.ToInt16(y *
+
+                Convert.ToSingle(200 / g.DpiX * 25.4)) +
+                Convert.ToInt16(200 / g.DpiX * 25.4));
+                if (ex != 0) { g.DrawLine(graphicsPen, old_ex, old_ey, ex, ey); }
+                old_ex = ex; old_ey = ey;
+                x = x + (Math.PI * 4) / WidthInMM;
+            }
+        }
+        // ********** Стандарт страничного пространства – Inch ******
+        private void Inches_Output(object sender, EventArgs e)
+        {
+            float ex = 0, old_ex = 0, old_ey = 0, ey = 0;
+            float x = 0, y = 0;
+            float shag = 0;
+            // Стандарт страничного пространства Inch
+            g.PageUnit = GraphicsUnit.Inch;
+            Pen axesPen = new Pen(Color.Orange, 0.01f);
+            Pen graphicsPen = new Pen(Color.FromArgb(255, 0, 0), 0.01f);
+            // Устанавливаем фон pictureBox1 в именованный цвет
+            pictureBox1.BackColor = Color.FromName("LightCyan");
+            pictureBox1.Refresh();
+            // Получаем ширину и высоту прямоугольника в дюймах
+            float WidthInInches = (pictureBox1.Size.Width - 1) / g.DpiX;
+            float HeightInInches = (pictureBox1.Size.Height - 1) / g.DpiY;
+            // Рисуем прямоугольник:
+            g.DrawRectangle(axesPen, 0, 0, WidthInInches, HeightInInches);
+            // Рисуем оси
+            g.DrawLine(axesPen, 0, HeightInInches / 2, WidthInInches,
+            HeightInInches / 2);
+
+            g.DrawLine(axesPen, WidthInInches / 2, 0, WidthInInches / 2,
+            HeightInInches);
+
+            // Рисуем график функции y=Sin(x)
+            x = -Convert.ToSingle(Math.PI * 2);
+            ex = 0;
+            shag = Convert.ToSingle(WidthInInches / 25.4);
+            while (ex <= WidthInInches + shag)
+            {
+                y = Convert.ToSingle(Math.Pow(x, 3) + 2 * Math.Pow(x, 2) + x);
+                ey = (Convert.ToSingle(-y) + HeightInInches / 2);
+                if (ex != 0) { g.DrawLine(graphicsPen, old_ex, old_ey, ex, ey); }
+                old_ex = ex; old_ey = ey;
+                ex = ex + shag;
+                x = x + Convert.ToSingle((Math.PI * 4) / shag);
+            }
+        }
+        // ****** Очистка PictureBox1 цветом, созданным пользователем **
+        private void Clear_PictureBox(object sender, EventArgs e)
+        {
+            g.Clear(Color.FromArgb(224, 224, 224));
+        }
+    }
+}
