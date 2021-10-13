a6c552f20fd5e354977a3579612f158ae3bb7325
Близорукость <90715911+miopiakrya@users.noreply.github.com>
2021-10-13 15:09:40 +0300

    compgraf

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace CompGraf.Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(768, 995);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(777, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Запись в файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(777, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Очистка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(777, 403);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Отображение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 1007);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная Работа №3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace CompGraf.Lab3
+{
+    public partial class Form1 : Form
+    {
+        // Объявляем объект "g" класса Graphics
+        Graphics g;
+        string filename = @"Strings.txt";
+        string[] sm = {
+        "First line", "Second line", "Third line",
+        "Fourth line", "Fifth line", "Sixth line",
+        "Seventh line", "Eighth line", "Ninth line",
+        "Tenth line", "Eleven line", "Twelve line",
+        "Thirteenth line", "Fourteenth line",
+        "Fifteenth line"};
+        public Form1()
+        {
+            InitializeComponent();
+            // Предоставляем объекту "g" класса Graphics возможность
+            // рисования на pictureBox1:
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // **************** Запись в файл ********************
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter f = new StreamWriter(new FileStream(filename,
+            FileMode.Create, FileAccess.Write));
+            foreach (string s in sm) { f.WriteLine(s); }
+            f.Close();
+            MessageBox.Show("15 строк записано в файл !");
+        }
+
+        // *************** Очистка файла и PictureBox **********
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.FromArgb(255, 255, 255));
+            File.Delete(filename);
+            MessageBox.Show("Файл Strings.txt удалён !");
+        }
+
+        // *************** Отображение строк текста *************
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            // Чтение строк из файла
+            try
+            {
+                StreamReader f = new StreamReader(new FileStream(filename,
+                FileMode.Open, FileAccess.Read));
+                for (int i = 0; i < 15; i++) { sm[i] = f.ReadLine(); }
+                f.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            // **** Отображение строк разными шрифтами и выравниванием **
+            pictureBox1.BackColor = Color.FromName("Azure");
+            pictureBox1.Refresh();
+            for (int i = 0; i < 15; i++)
+            {
+                // Отображение первой группы строк
+                if ((i >= 0) && (i < 6))
+                {
+                    Font fn = new Font("Imprint MT Shadow", 24, FontStyle.Regular);
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.Alignment = StringAlignment.Near;
+                    sf.LineAlignment = StringAlignment.Far;
+                    g.DrawString(sm[i], fn, Brushes.DarkGray,
+                    new RectangleF(0, 0 + i * 30, pictureBox1.Size.Width - 20,
+                    pictureBox1.Size.Height - 150), sf);
+                    fn.Dispose();
+                }
+                // Отображение второй группы строк
+                if ((i >= 6) && (i < 13))
+                {
+                    k = i - 9;
+                    Font fn = new Font("Arial Black", 18, FontStyle.Bold);
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Far;
+                    g.DrawString(sm[i], fn, Brushes.Firebrick,
+                    new RectangleF(0, 0 + k * 24, pictureBox1.Size.Width - 20,
+                    pictureBox1.Size.Height - 150), sf);
+                    fn.Dispose();
+                }
+                // Отображение третьей группы строк
+                if (i == 13)
+                {
+                    Font fn = new Font("Corbel", 1.5f, FontStyle.Italic, GraphicsUnit.Inch);
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.FormatFlags = StringFormatFlags.DirectionVertical;
+                    sf.Alignment = StringAlignment.Near;
+                    sf.LineAlignment = StringAlignment.Center;
+                    g.DrawString(sm[i], fn, Brushes.DeepPink,
+                    new RectangleF(0, 0 + i * 4, pictureBox1.Size.Width - 100,
+                    pictureBox1.Size.Height - 20), sf);
+                    fn.Dispose();
+
+                }
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
